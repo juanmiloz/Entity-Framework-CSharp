536e9c75dd42e962e680d5c334a4a7c1fed2e2fa
jczorrilla <jczorrilla@tecnoquimicas.com>
2024-02-29 15:23:44 -0500

    update repo

--- obj/Debug/net8.0/Fundamentos de Entity-Framework.AssemblyInfo.cs
+++ obj/Debug/net8.0/Fundamentos de Entity-Framework.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fundamentos de Entity-Framework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58baa6a279e07d26336d737c832cef62c0b538f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fundamentos de Entity-Framework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fundamentos de Entity-Framework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
